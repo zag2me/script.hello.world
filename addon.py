019a13246dfcb5b3ec27d434243523e3a9833447
Zag2me <doowles2@hotmail.com>
2015-10-20 12:21:38 +0100

    Initial version of script

---
+++ addon.py
@@ -0,0 +1,11 @@
+﻿import xbmcaddon
+import xbmcgui
+ 
+addon       = xbmcaddon.Addon()
+addonname   = addon.getAddonInfo('name')
+ 
+line1 = "Hello World!"
+line2 = "We can write anything we want here"
+line3 = "Using Python"
+ 
+xbmcgui.Dialog().ok(addonname, line1, line2, line3)
